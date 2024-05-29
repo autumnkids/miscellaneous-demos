913803c1b9d40917c4c010e01224297f362f5d78
Yinlin Zhou <kelonzhou@gmail.com>
2024-05-28 22:39:48 -0400

    Add get api load test example

---
+++ k6/rest-service-example/load-tests/load-getHelloWorld.js
@@ -0,0 +1,22 @@
+import http from 'k6/http';
+import { check, sleep } from 'k6';
+
+export const options = {
+  stages: [
+    { duration: '10s', target: 5 },
+    { duration: '10s', target: 10 },
+    { duration: '10s', target: 5 }
+  ],
+  thresholds: {
+    http_req_duration: ['p(99) < 100'],
+  }
+};
+
+export default function() {
+  const response = http.get(`${__ENV.TEST_HOST}/api/v1/examples/helloworld`);
+  check(response, {
+    'status code is 200': r => r.status === 200,
+    'body contains hello world': r => r.body.includes(`{"message":"Hello World!"}`),
+  });
+  sleep(1);
+}
