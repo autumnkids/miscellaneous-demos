c5f05cdbd7cb28812fe6cb2fe810476a0f29920e
Yinlin Zhou <kelonzhou@gmail.com>
2024-06-25 22:51:32 -0400

    Add integration test with testcontainers

--- testcontainers/consumer-service/src/main/java/io/autumnkids/consumer_service/greeting/GreetService.java
+++ testcontainers/consumer-service/src/main/java/io/autumnkids/consumer_service/greeting/GreetService.java
@@ -1,5 +1,7 @@
 package io.autumnkids.consumer_service.greeting;
 
+import io.autumnkids.consumer_service.upstream_rest_service.UpstreamRestServiceProperties;
+import org.openapitools.client.ApiClient;
 import org.openapitools.client.ApiException;
 import org.openapitools.client.api.HelloWorldControllerApi;
 import org.openapitools.client.model.GreetResponse;
@@ -8,7 +10,14 @@ import org.springframework.stereotype.Service;
 @Service
 public class GreetService {
 
-  private static final HelloWorldControllerApi helloWorldApi = new HelloWorldControllerApi();
+  private final ApiClient client;
+
+  private final HelloWorldControllerApi helloWorldApi;
+
+  public GreetService(UpstreamRestServiceProperties properties) {
+    client = new ApiClient().setBasePath(String.format("http://%s:%d", properties.getHost(), properties.getPort()));
+    helloWorldApi = new HelloWorldControllerApi(client);
+  }
 
   public String greet(String name) throws ApiException {
     GreetResponse response = helloWorldApi.greet(name);
--- testcontainers/consumer-service/src/test/java/io/autumnkids/consumer_service/GreetControllerTest.java
+++ testcontainers/consumer-service/src/test/java/io/autumnkids/consumer_service/GreetControllerTest.java
@@ -2,6 +2,7 @@ package io.autumnkids.consumer_service;
 
 import io.autumnkids.consumer_service.greeting.GreetController;
 import io.autumnkids.consumer_service.greeting.GreetService;
+import io.autumnkids.consumer_service.upstream_rest_service.UpstreamRestServiceProperties;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -11,19 +12,28 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.DockerImageName;
 
 @WebMvcTest(GreetController.class)
+@Testcontainers
 class GreetControllerTest {
 
 	@Autowired
 	private MockMvc mockMvc;
+
+	@Container
+	static GenericContainer<?> upstreamService = new GenericContainer<>(DockerImageName.parse("upstream-rest-service-service:latest"))
+			.withExposedPorts(9001);
 
 	@Test
 	void contextLoads() throws Exception {
 		this.mockMvc.perform(MockMvcRequestBuilders.get("/api/v1/greet").param("name", "Test"))
 				.andDo(MockMvcResultHandlers.print())
 				.andExpect(MockMvcResultMatchers.status().isOk())
-				.andExpect(MockMvcResultMatchers.content().string("{\"message\": \"Hello Test!\"}"));
+				.andExpect(MockMvcResultMatchers.content().string("{\"message\":\"Hello Test!\"}"));
 	}
 
 	@TestConfiguration
@@ -31,7 +41,11 @@ class GreetControllerTest {
 
 		@Bean
 		GreetService getRealGreetServiceImpl() {
-			return new GreetService();
+			upstreamService.start();
+			UpstreamRestServiceProperties properties = new UpstreamRestServiceProperties();
+			properties.setHost(upstreamService.getHost());
+			properties.setPort(upstreamService.getFirstMappedPort());
+			return new GreetService(properties);
 		}
 	}
 
